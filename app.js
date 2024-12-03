3c068bb56344849d3162705084ddaf57a2505554
Artem-NA <artemkh16@gmail.com>
2024-12-04 00:59:08 +0200

    ok

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ const notFoundHandler = (req, res, next) => {
 };
 app.use(notFoundHandler);
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
